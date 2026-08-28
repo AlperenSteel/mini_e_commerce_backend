ae6b41aabc40d3d343ecd12f112d9ebf6cf02f90
Alperen <y.celik2022@gtu.edu.tr>
2026-08-28 15:05:36 +0300

    deletion reverted

--- src/main/java/com/example/minicommerce/repository/OrderRepository.java
+++ src/main/java/com/example/minicommerce/repository/OrderRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 //TODO
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByUser(User user);
 }
