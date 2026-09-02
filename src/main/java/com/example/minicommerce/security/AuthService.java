519fd5f66cd3a4f2ba3175fe189eb8264a705f08
Alperen <y.celik2022@gtu.edu.tr>
2026-09-02 13:27:03 +0300

    JwtService

--- src/main/java/com/example/minicommerce/security/AuthService.java
+++ src/main/java/com/example/minicommerce/security/AuthService.java
@@ -1,4 +1,30 @@
 package com.example.minicommerce.security;
 
+import com.example.minicommerce.entity.User;
+import com.example.minicommerce.repository.RefreshTokenRepository;
+import com.example.minicommerce.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository,
+                       RefreshTokenRepository refreshTokenRepository,
+                       JwtService jwtService,
+                       PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    public
+
+
 }
--- src/main/java/com/example/minicommerce/security/JwtService.java
+++ src/main/java/com/example/minicommerce/security/JwtService.java
@@ -1,4 +1,61 @@
 package com.example.minicommerce.security;
 
+import com.example.minicommerce.entity.User;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+
+@Service
 public class JwtService {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.access-expiration}")
+    private long accessExpiration;
+
+    @Value("${jwt.refresh-expiration}")
+    private long refreshExpiration;
+
+
+    // String olarak değil de SecretKey tipinde istiyor
+    private SecretKey getSigningKey(){
+        byte[] keyBytes =Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    public String generateAccessToken(User user) {
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .claim("role", user.getRole().name())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + accessExpiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+    public String extractUsername(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+    public boolean isTokenValid(String token){
+        try{
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException e){
+            return false;
+        }
+
+    }
 }
