8c57259bd8155125b2f583bbdcbeded4a649162c
Alperen <y.celik2022@gtu.edu.tr>
2026-09-02 16:06:16 +0300

    Auth Controller

--- src/main/java/com/example/minicommerce/controller/AuthController.java
+++ src/main/java/com/example/minicommerce/controller/AuthController.java
@@ -1,5 +1,31 @@
 package com.example.minicommerce.controller;
 
+import com.example.minicommerce.dto.auth.AuthResponse;
+import com.example.minicommerce.dto.auth.LoginRequest;
+import com.example.minicommerce.dto.auth.RegisterRequest;
+import com.example.minicommerce.security.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+    @PostMapping("/register")
+    public AuthResponse register(@Valid @RequestBody  RegisterRequest registerRequest){
+
+        return authService.register(registerRequest);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@Valid @RequestBody LoginRequest loginRequest){
+
+        return authService.login(loginRequest);
+    }
 
 }
