77e24d1dda1816ecdb3031bd9152279a41f84a57
Alperen <y.celik2022@gtu.edu.tr>
2026-09-03 13:44:13 +0300

    JwtFilter.java added

--- src/main/java/com/example/minicommerce/security/JwtFilter.java
+++ src/main/java/com/example/minicommerce/security/JwtFilter.java
@@ -1,4 +1,58 @@
 package com.example.minicommerce.security;
 
-public class JwtFilter {
+
+import com.example.minicommerce.entity.User;
+import com.example.minicommerce.exception.ResourceNotFoundException;
+import com.example.minicommerce.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter{
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+
+    public JwtFilter(JwtService jwtService, UserRepository userRepository){
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = authHeader.substring(7);
+        if (!jwtService.isTokenValid(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String username = jwtService.extractUsername(token);
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("Kullanıcı bulunamadı"));
+
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+        
+        filterChain.doFilter(request, response);
+    }
+
+
+
 }
